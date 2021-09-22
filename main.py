fd694a5aaa81405f1a7b2f74536d0d13e96c52ec
sashaberger31 <sashaberger31415@gmail.com>
2021-09-21 21:42:47 -0700

    Added basic edge code with tracker image

---
+++ main.py
@@ -0,0 +1,18 @@
+import cv2
+import argparse
+import numpy as np
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--image", required = True)
+args = vars(parser.parse_args())
+image = cv2.imread(args["image"])
+cv2.imshow("Image", image)
+cv2.waitKey(0)
+image = cv2.imread(args["image"])
+image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+image = cv2.GaussianBlur(image, (5, 5), 0)
+cv2.imshow("Blurred", image)
+canny = cv2.Canny(image, 30, 150)
+cv2.imshow("Canny", canny)
+cv2.waitKey(0)
